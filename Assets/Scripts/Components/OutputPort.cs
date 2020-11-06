44ec7a0e5cac7ef11f76d41906cb777ea9b52bd4
rubix222 <rubix222@gmail.com>
2020-11-05 22:48:13 -0500

    Stubbed out doors and locks

---
+++ Assets/Scripts/Components/OutputPort.cs
@@ -0,0 +1,12 @@
+﻿using NoZ;
+using Puzzled;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Puzzled
+{
+    public class OutputPort : ActorComponent
+    {
+    }
+}
---
+++ Assets/Scripts/Components/PressurePlate.cs
@@ -0,0 +1,19 @@
+﻿using NoZ;
+using Puzzled;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Puzzled
+{
+    public class PressurePlate : ActorComponent
+    {
+        [SerializeField] private bool isSticky = false;
+
+        [ActorEventHandler]
+        private void OnEnter(EnterCellEvent evt)
+        {
+            // do stuff
+        }
+    }
+}
