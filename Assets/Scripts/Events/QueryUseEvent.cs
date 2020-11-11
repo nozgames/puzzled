f0a395d49c8c70f54206f00ff83b3ace5011a46f
rubix222 <rubix222@gmail.com>
2020-11-10 22:01:23 -0500

    use events

---
+++ Assets/Scripts/Events/QueryUseEvent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using NoZ;
+
+namespace Puzzled
+{
+    class QueryUseEvent : ActorEvent
+    {
+        public Actor user { get; private set; }
+
+        private bool _result = false;
+
+        public bool result
+        {
+            get => _result;
+            set
+            {
+                _result = value;
+                if (_result)
+                    IsHandled = true;
+            }
+        }
+
+        public QueryUseEvent(Actor user)
+        {
+            this.user = user;
+            result = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Events/UseEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using NoZ;
+
+namespace Puzzled
+{
+    class UseEvent : ActorEvent
+    {
+        public Actor user { get; private set; }
+
+        public UseEvent(Actor user)
+        {
+            this.user = user;
+        }
+    }
+}
