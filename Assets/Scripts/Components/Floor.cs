472d028076952c621d298ba54ff3c21fb3ce5390
Bryan Dube <bryan.dube@resultstack.com>
2020-12-06 07:33:18 -0600

    Alternating floor tile visuals

---
+++ Assets/Scripts/Components/Floor.cs
@@ -0,0 +1,19 @@
+﻿using NoZ;
+using UnityEngine;
+
+namespace Puzzled
+{
+    public class Floor : TileComponent
+    {
+        [SerializeField] private GameObject visualDk;
+        [SerializeField] private GameObject visualLt;
+
+        [ActorEventHandler]
+        private void OnCellChanged(CellChangedEvent evt)
+        {
+            var dark = (tile.cell.y + tile.cell.x) % 2 == 0;
+            visualDk.SetActive(dark);
+            visualLt.SetActive(!dark);
+        }
+    }
+}
---
+++ Assets/Scripts/Events/CellChangedEvent.cs
@@ -0,0 +1,14 @@
+﻿using NoZ;
+
+namespace Puzzled
+{
+    class CellChangedEvent : ActorEvent
+    {
+        public Tile tile { get; set; }
+
+        public CellChangedEvent(Tile tile)
+        {
+            this.tile = tile;
+        }
+    }
+}
