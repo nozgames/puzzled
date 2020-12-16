b76f1ed33adc09d6766fd66e2c909e43821c0bdc
rubix222 <rubix222@gmail.com>
2020-12-16 00:00:40 -0500

    Initial Camera and zoom control

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -12,7 +12,15 @@ namespace Puzzled
         [SerializeField] private Camera _camera = null;
         [SerializeField] private float _transitionSpeed = 1;
 
+        private Cell _cell = Cell.invalid;
+        private int _zoomLevel;
+
         private static CameraManager _instance = null;
+
+        private void Awake()
+        {
+            _zoomLevel = (int)_camera.orthographicSize;    
+        }
 
         private void OnEnable()
         {
@@ -24,19 +32,64 @@ namespace Puzzled
             _instance = null;
         }
 
-        public static void TransitionToTile(Cell cell)
+        public static void JumpToTile(Cell cell, int zoomLevel)
         {
+            _instance._camera.transform.position = TileGrid.CellToWorld(cell);
+            _instance._camera.orthographicSize = zoomLevel / 2;
+
+            _instance._cell = cell;
+            _instance._zoomLevel = zoomLevel;
+        }
+
+        public static void TransitionToCell(Cell cell, int zoomLevel)
+        {
+            if ((cell == _instance._cell) && (zoomLevel == _instance._zoomLevel))
+                return; // nothing to do
+
             GameManager.busy++;
-           
-            Tween.Move(_instance._camera.transform.position, TileGrid.CellToWorld(cell), false)
-                    .Duration(_instance._transitionSpeed)
-                    .OnStop(_instance.OnCameraTransitionComplete)
-                    .Start(_instance._camera.gameObject);
+
+            var tweenGroup = Tween.Group();
+
+            if (zoomLevel != _instance._zoomLevel)
+                tweenGroup.Child(Tween.Custom(_instance.CameraZoomUpdate, new Vector4(_instance._zoomLevel, zoomLevel, 0), Vector4.zero));
+
+            if (cell != _instance._cell)
+                tweenGroup.Child(Tween.Move(TileGrid.CellToWorld(_instance._cell), TileGrid.CellToWorld(cell), false));
+
+            tweenGroup.Duration(_instance._transitionSpeed)
+                .OnStop(_instance.OnCameraTransitionComplete)
+                .Start(_instance._camera.gameObject);
+
+            _instance._cell = cell;
+            _instance._zoomLevel = zoomLevel;
+        }
+
+        private bool CameraZoomUpdate(Tween tween, float t)
+        {
+            _camera.orthographicSize = Mathf.Lerp(tween.Param1.x, tween.Param1.y, t) / 2;
+
+            return true;
         }
 
         private void OnCameraTransitionComplete()
         {
             GameManager.busy--;
+        }
+
+        public static void Play()
+        {
+            if (_instance._cell == Cell.invalid)
+            {
+                if (GameManager.playerCell != Cell.invalid)
+                    _instance._cell = GameManager.playerCell;
+                else
+                    _instance._cell = TileGrid.WorldToCell(_instance._camera.transform.position);
+            }
+        }
+
+        public static void Stop()
+        {
+
         }
     }
 }
--- Assets/Scripts/Components/StaticCamera.cs
+++ Assets/Scripts/Components/StaticCamera.cs
@@ -5,6 +5,20 @@ namespace Puzzled
 {
     class StaticCamera : TileComponent
     {
+        const int MinZoomLevel = 2;
+        const int MaxZoomLevel = 20;
+
+        [Editable]
+        public int zoomLevel 
+        { 
+            get => _zoomLevel; 
+            private set => _zoomLevel = Mathf.Clamp(value, MinZoomLevel, MaxZoomLevel); 
+        }
+
+        private int _zoomLevel = 12;
+
+        [SerializeField] private bool isInitialLocation = false;
+
         [ActorEventHandler]
         private void OnActivateWire(WireActivatedEvent evt)
         {
@@ -13,7 +27,14 @@ namespace Puzzled
 
         private void ActivateCamera()
         {
-            CameraManager.TransitionToTile(tile.cell);
+            CameraManager.TransitionToCell(tile.cell, zoomLevel);
+        }
+
+        [ActorEventHandler]
+        private void OnStart(StartEvent evt)
+        {
+            if (isInitialLocation)
+                CameraManager.JumpToTile(tile.cell, zoomLevel);
         }
     }
 }
