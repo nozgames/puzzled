9fd32c12ba37e3d789b92134fa3015dd61f5fb97
rubix222 <rubix222@gmail.com>
2020-12-23 01:28:42 -0500

    Delay tiles

---
+++ Assets/Scripts/Components/Delay.cs
@@ -0,0 +1,51 @@
+﻿using NoZ;
+using UnityEngine;
+
+namespace Puzzled
+{
+    class Delay : TileComponent
+    {
+        [Editable]
+        public int delayTicks { get; private set; }
+
+        private bool isDelaying = false;
+        private bool wasDelaying = false;
+        private int tickCount = 0;
+
+        [ActorEventHandler]
+        private void OnActivateWire(WireActivatedEvent evt)
+        {
+            tickCount = 0;
+            isDelaying = true;
+            wasDelaying = true;
+        }
+
+        [ActorEventHandler]
+        private void OnDeactivateWire(WireDeactivatedEvent evt)
+        {
+            tickCount = 0;
+            isDelaying = false;
+            wasDelaying = false;
+
+            tile.SetOutputsActive(false);
+        }
+
+        [ActorEventHandler]
+        private void OnTickStart(TickEvent evt) 
+        {
+            if (!isDelaying)
+                return;
+
+            if (wasDelaying)
+                ++tickCount;
+
+            wasDelaying = isDelaying;
+
+            if (tickCount >= delayTicks)
+            {
+                tile.SetOutputsActive(true);
+                isDelaying = false;
+            }
+        }
+    }
+}
