54176eb7897da6039339c0a736635e8bcb97a6ca
rubix222 <rubix222@gmail.com>
2020-12-23 01:01:26 -0500

    State Cycle

---
+++ Assets/Scripts/Components/StateCycle.cs
@@ -0,0 +1,80 @@
+﻿using NoZ;
+using UnityEngine;
+
+namespace Puzzled
+{
+    class StateCycle : TileComponent
+    {
+        private int stateIndex;
+
+        private bool isCycling { get; set; } = false;
+        private bool wasCycling { get; set; } = false;
+
+        [Editable]
+        public bool clearOnDeactivate { get; set; } = true;
+
+        [Editable(hidden = true)]
+        public string[] steps { get; set; }
+
+        [ActorEventHandler]
+        private void OnActivateWire(WireActivatedEvent evt)
+        {
+            UpdateCyclingState();
+        }
+
+        [ActorEventHandler]
+        private void OnDeactivateWire(WireDeactivatedEvent evt)
+        {
+            UpdateCyclingState();
+        }
+
+        [ActorEventHandler]
+        private void OnStart(StartEvent evt) => UpdateOutputWires();
+
+        [ActorEventHandler]
+        private void OnTickStart(TickEvent evt)
+        {
+            if (!isCycling)
+            {
+                wasCycling = false;
+                return;
+            }
+
+            if (tile.outputCount == 0)
+                return;
+
+            if (wasCycling)
+                stateIndex = (stateIndex + 1) % steps.Length;
+
+            wasCycling = isCycling;
+
+            UpdateOutputWires();
+        }
+
+        private void UpdateCyclingState()
+        {
+            isCycling = tile.hasActiveInput;
+
+            if (!isCycling && clearOnDeactivate)
+            {
+                stateIndex = 0;
+                UpdateOutputWires();
+            }
+        }
+
+        private void UpdateOutputWires()
+        {
+            if (!isCycling && clearOnDeactivate)
+            {
+                tile.SetOutputsActive(false);
+                return;
+            }
+
+            for (int i = 0; i < tile.outputCount; ++i)
+            {
+                bool isWireActive = ((tile.GetOutputOption(i, 0) & (1 << stateIndex)) != 0);
+                tile.outputs[i].enabled = isWireActive;
+            }
+        }
+    }
+}
