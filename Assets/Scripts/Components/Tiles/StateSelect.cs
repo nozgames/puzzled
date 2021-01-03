5d483789360f28758fc0ce9e55458f502a92773f
Bryan Dube <bryan.dube@resultstack.com>
2021-01-02 20:54:37 -0600

    More fixes

--- Assets/Scripts/Components/Tiles/StateSelect.cs
+++ Assets/Scripts/Components/Tiles/StateSelect.cs
@@ -12,32 +12,32 @@ namespace Puzzled
         /// </summary>
         [Editable]
         [Port(PortFlow.Input, PortType.Number, legacy = true)]
-        public Port valueIn { get; set; }
-
-        /// <summary>
-        /// Output used to forward power to the current state
-        /// </summary>
-        [Editable]
-        [Port(PortFlow.Output, PortType.Power, legacy = true)]
-        public Port powerOut { get; set; }
+        private Port valueInPort { get; set; }
 
         /// <summary>
         /// Output used to send the current selected state index
         /// </summary>
         [Editable]
         [Port(PortFlow.Output, PortType.Number)]
-        public Port valueOut { get; set; }
+        private Port valueOutPort { get; set; }
+
+        /// <summary>
+        /// Output used to forward power to the current state
+        /// </summary>
+        [Editable]
+        [Port(PortFlow.Output, PortType.Power, legacy = true)]
+        private Port powerOutPort { get; set; }
 
         [ActorEventHandler]
         private void OnValueSignalEvent (ValueEvent evt) => UpdateOutput(evt.value);
 
         private void UpdateOutput(int value)
         {
-            valueOut.SendValue(value);
+            valueOutPort.SendValue(value);
 
             var stateIndex = value - 1;
-            for (int i = 0; i < powerOut.wireCount; ++i)
-                powerOut.SetPowered(i, (powerOut.GetWireOption(i, 0) & (1 << stateIndex)) != 0);
+            for (int i = 0; i < powerOutPort.wireCount; ++i)
+                powerOutPort.SetPowered(i, (powerOutPort.GetWireOption(i, 0) & (1 << stateIndex)) != 0);
         }
     }
 }
