35d3abc40507227ea37d6b51fd555961fe3e08c8
Bryan Dube <bryan.dube@resultstack.com>
2021-01-12 08:57:57 -0600

    Chests

--- Assets/Scripts/Components/Tiles/Chest.cs
+++ Assets/Scripts/Components/Tiles/Chest.cs
@@ -13,48 +13,54 @@ namespace Puzzled
         public System.Guid prizeItem { get; private set; }
 
         [Header("Visuals")]
-        [SerializeField] private GameObject lockedVisual;
-        [SerializeField] private GameObject unlockedVisual;
+        [SerializeField] private Animator _animator = null;
 
-        private bool locked = false;
-
-        
+        private bool _locked = false;
+        private bool _used = false;
+       
         private void Start()
         {
-            locked = keyItem != null;
-            UpdateVisuals();
-        }
-
-        private void UpdateVisuals()
-        {
-            lockedVisual.SetActive(locked);
-            unlockedVisual.SetActive(!locked);
+            _locked = keyItem != null;
         }
 
         [ActorEventHandler]
         private void OnUse(UseEvent evt)
         {
+            if (_used)
+                return;
+
             // Always retport we were used, even if the use fails
             evt.IsHandled = true;
 
-            if(locked)
+            if(_locked)
             {
                 // Check to see if the user has the item to unlock the chest
-                locked = !evt.user.Send(new QueryHasItemEvent(keyItem.guid));
-                if (locked)
+                _locked = !evt.user.Send(new QueryHasItemEvent(keyItem.guid));
+                if (_locked)
                     return;
-
-                if(_unlockSound != null)
-                    AudioManager.Instance.Play(_unlockSound);
             }
 
-            var cell = tile.cell;
+            _used = true;
 
-            // Destroy ourself first or the new item cannot spawn 
-            tile.Destroy();
+            PlaySound(_unlockSound);
 
-            // Spawn the prize at the same spot
-            puzzle.InstantiateTile(TileDatabase.GetTile(prizeItem), cell);
+            _animator.SetTrigger("Open");
+
+            GameManager.busy++;
+
+            Tween.Wait(0.5f)
+                .OnStop(() => {
+                    var cell = tile.cell;
+
+                    // Destroy ourself first or the new item cannot spawn 
+                    //tile.Destroy();
+
+                    // Spawn the prize at the same spot
+                    puzzle.InstantiateTile(TileDatabase.GetTile(prizeItem), cell);
+
+                    GameManager.busy--;
+                })
+                .Start(gameObject);
         }
     }
 }
