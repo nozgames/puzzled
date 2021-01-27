743d053d7b74a473bb7d9ea61c2c61c59b3d7a1f
rubix222 <rubix222@gmail.com>
2021-01-27 00:40:25 -0500

    Multi Page Sign hookup

--- Assets/Scripts/UI/Popups/UIPopupText.cs
+++ Assets/Scripts/UI/Popups/UIPopupText.cs
@@ -2,13 +2,34 @@
 
 namespace Puzzled
 {
-    public class UIPopupText : MonoBehaviour
+    public class UIPopupText : UIPopup
     {
         [SerializeField] private TMPro.TextMeshProUGUI _text = null;
 
-        public string text {
-            get => _text.text;
-            set => _text.text = value;
+        private string[] _pages;
+        private int pageIndex = 0;
+
+        public string[] pages
+        { 
+            get => _pages;
+            set
+            {
+                pageIndex = 0;
+                _pages = value;
+                _text.text = _pages[pageIndex];
+            }
+        }
+
+        public override void Use()
+        {
+            ++pageIndex;
+            if (pageIndex >= pages.Length)
+            {
+                Close();
+                return;
+            }
+
+            _text.text = _pages[pageIndex];
         }
     }
 }
