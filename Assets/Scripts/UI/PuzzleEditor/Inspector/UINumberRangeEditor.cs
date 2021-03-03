437e65c19c45013fce7f8bda97cc4615f502fe60
Bryan Dube <bryan.dube@resultstack.com>
2021-03-02 20:48:07 -0600

    Added input box to slider properties

--- Assets/Scripts/UI/PuzzleEditor/Inspector/UINumberRangeEditor.cs
+++ Assets/Scripts/UI/PuzzleEditor/Inspector/UINumberRangeEditor.cs
@@ -7,10 +7,18 @@ namespace Puzzled.Editor
     public class UINumberRangeEditor : UIPropertyEditor
     {
         [SerializeField] private UISlider _slider = null;
+        [SerializeField] private TMPro.TMP_InputField _input = null;
 
         private void Awake()
         {            
             _slider.onEndDrag += OnCommitValue;
+            _input.onSubmit.AddListener(OnInputChanged);
+            _input.onEndEdit.AddListener(OnInputChanged);
+        }
+
+        private void OnInputChanged(string value)
+        {
+            _slider.value = float.TryParse(value, out var parsed) ? parsed : 0;
         }
 
         private void OnCommitValue()
@@ -20,6 +28,7 @@ namespace Puzzled.Editor
 
         private void OnValueChanged (float value)
         {
+            _input.SetTextWithoutNotify(((int)value).ToString());
             target.tile.SetPropertyValue(target.tileProperty.name, (int)_slider.value);
         }
 
@@ -30,6 +39,8 @@ namespace Puzzled.Editor
             _slider.maxValue = target.tileProperty.editable.range.y;
             _slider.SetValueWithoutNotify(target.GetValue<int>());
             _slider.onValueChanged.AddListener(OnValueChanged);
+
+            _input.SetTextWithoutNotify(((int)_slider.value).ToString());
         }
     }
 }
