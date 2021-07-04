f972436a0dcecd356f887d2e189cb19feb97d750
Bryan Dube <bdube@nozgames.com>
2021-07-04 14:04:23 -0500

    Sound
    - reorganized the sounds folder
    - added more footstep sounds
    - hooked up footstep sounds per floor to the player

--- Assets/Scripts/Tiles/Floor.cs
+++ Assets/Scripts/Tiles/Floor.cs
@@ -5,21 +5,8 @@ namespace Puzzled
 {
     public class Floor : TileComponent
     {
-        [ActorEventHandler]
-        private void OnStart(StartEvent evt) => UpdateVisuals();
+        [SerializeField] private AudioClip _footstepClip = null;
 
-        [ActorEventHandler]
-        private void OnCellChanged(CellChangedEvent evt) => UpdateVisuals();
-
-        private void UpdateVisuals()
-        {
-            if (!tile.isLinked)
-                return;
-
-            //var state = Random.state;
-            //Random.InitState(tile.cell.x * tile.cell.y);
-            //_visual.localRotation = Quaternion.Euler(0, 90.0f * Random.Range(0, 4), 0.0f);
-            //Random.state = state;
-        }
+        public AudioClip footstepClip => _footstepClip;
     }
 }
